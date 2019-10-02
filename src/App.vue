3bee8fdfe69d76149e1953141a870fe958d3ee17
Jonthan Schwarzenböck <jonathan@lightsnowdev.com>
2019-10-02 15:37:35 +0200

    add fetch api to get users from server

--- src/App.vue
+++ src/App.vue
@@ -41,60 +41,24 @@
 <script>
 export default {
   name: "App",
+  async mounted() {
+    this.loadUsers();
+  },
   data: () => ({
-    users: [
-      {
-        id: 1,
-        name: "Leanne Graham",
-        username: "Bret",
-        email: "Sincere@april.biz",
-        address: {
-          street: "Kulas Light",
-          suite: "Apt. 556",
-          city: "Gwenborough",
-          zipcode: "92998-3874",
-          geo: {
-            lat: "-37.3159",
-            lng: "81.1496"
-          }
-        },
-        phone: "1-770-736-8031 x56442",
-        website: "hildegard.org",
-        company: {
-          name: "Romaguera-Crona",
-          catchPhrase: "Multi-layered client-server neural-net",
-          bs: "harness real-time e-markets"
-        }
-      },
-      {
-        id: 2,
-        name: "Leanne Graham",
-        username: "Bret",
-        email: "Sincere@april.biz",
-        address: {
-          street: "Kulas Light",
-          suite: "Apt. 556",
-          city: "Gwenborough",
-          zipcode: "92998-3874",
-          geo: {
-            lat: "-37.3159",
-            lng: "81.1496"
-          }
-        },
-        phone: "1-770-736-8031 x56442",
-        website: "hildegard.org",
-        company: {
-          name: "Romaguera-Crona",
-          catchPhrase: "Multi-layered client-server neural-net",
-          bs: "harness real-time e-markets"
-        }
-      }
-    ],
+    users: [],
     items: [
       { title: "Dashboard", icon: "mdi-view-dashboard" },
       { title: "Photos", icon: "mdi-image" },
       { title: "About", icon: "mdi-help-box" }
     ]
-  })
+  }),
+  methods: {
+    loadUsers() {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then(stream => stream.json())
+        .then(data => (this.users = data))
+        .catch(error => console.error(error));
+    }
+  }
 };
 </script>
