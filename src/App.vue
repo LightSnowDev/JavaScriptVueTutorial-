fd21559b3d914c1f7cc737fa7ab188330267b215
Jonthan Schwarzenböck <jonathan@lightsnowdev.com>
2019-10-02 15:23:34 +0200

    add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <template>
   <v-app>
-    
+    <v-navigation-drawer permanent app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">Application</v-list-item-title>
+          <v-list-item-subtitle>subtext</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <v-container fluid>hi</v-container>
+    </v-content>
   </v-app>
 </template>
 
@@ -8,7 +33,11 @@
 export default {
   name: "App",
   data: () => ({
-    //
+    items: [
+      { title: "Dashboard", icon: "mdi-view-dashboard" },
+      { title: "Photos", icon: "mdi-image" },
+      { title: "About", icon: "mdi-help-box" }
+    ]
   })
 };
 </script>
